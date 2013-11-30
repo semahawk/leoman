6fc3022ab7d888b0ffc59e138de4ae3741f2efc3
Szymon Urbaś <szymon.urbas@aol.com>
2013-11-30 19:54:17 +0100

    Write the main asm file as well as Makefile.

---
+++ Makefile
@@ -0,0 +1,20 @@
+.PHONY: all run iso clean
+
+all: nihilum
+
+nihilum: nihilum.bin
+
+nihilum.bin: nihilum.asm
+	nasm -f bin -o nihilum.bin nihilum.asm
+	dd conv=notrunc if=nihilum.bin of=cdiso/nihilum.flp
+
+run:
+	qemu -fda cdiso/nihilum.flp
+
+iso: nihilum.bin
+	mkisofs -no-emul-boot -boot-load-size 4 -quiet -V 'Nihilum' -input-charset iso8859-1 -o cdiso/nihilum.iso -b nihilum.flp cdiso/
+
+clean:
+	rm -f cdiso/*.iso
+	rm -f *.bin
+
---
+++ nihilum.asm
@@ -0,0 +1,36 @@
+BITS 16
+
+start:
+  mov ax, 07C0h              ; set up 4k stack space after this bootloader
+  add ax, 288                ; (4096 + 512) / 16 bytes per paragraph
+  mov ss, ax
+  mov sp, 4096
+
+  mov ax, 07C0h              ; set data segment to where we're loaded
+  mov ds, ax
+
+  mov si, text_string        ; put string position into SI
+  call print_string          ; call our string-printing routine
+
+  jmp $                      ; infinity!
+
+  text_string db 'Nihilum says Salve ;)', 0
+
+print_string:
+  mov ah, 0Eh                ; routine: output string in SI to screen
+
+.repeat:
+  lodsb                      ; get character from string
+  cmp al, 0
+  je .done                   ; if char is zero, end of string
+  int 10h                    ; otherwise print it
+  jmp .repeat
+
+.done:
+  ret
+
+  times 510-($-$$) db 0      ; pad remainder of boot sector with 0s
+  dw 0xAA55                  ; the standard PC boot signature
+
+; vi: ft=nasm:ts=2:sw=2 expandtab
+
