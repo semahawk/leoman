9d266a02904e1c5a012dff0edf55c98539d0f921
Szymon Urbaś <szymon.urbas@aol.com>
2013-12-04 19:34:14 +0100

    Print the welcoming message 10 times.
    meh, an 'operating system'..

--- nihilum.asm
+++ nihilum.asm
@@ -9,12 +9,15 @@ start:
   mov ax, 07C0h              ; set data segment to where we're loaded
   mov ds, ax
 
+  mov cx, 10                 ; set the loop's counter to 10
+loop_string:
   mov si, text_string        ; put string position into SI
   call print_string          ; call our string-printing routine
+  loop loop_string           ; jump to loop_string if cx != 0
 
   jmp $                      ; infinity!
 
-  text_string db 'Nihilum says Salve ;)', 0
+  text_string db 'Nihilum says Salve ;)', 0ah, 0
 
 print_string:
   mov ah, 0Eh                ; routine: output string in SI to screen
