bcc4a2f94052a8673b5f84d3c272632e1e019330
Szymon Urbaś <szymon.urbas@aol.com>
2013-12-08 01:27:21 +0100

    The 'print' macros are preserving the registers.
    Or at least ax, bx, cx and dx :P

--- utils.asm
+++ utils.asm
@@ -1,14 +1,33 @@
 ; a few handy macros
 
+; saves the registers
+%macro regsave 0
+  push ax
+  push bx
+  push cx
+  push dx
+%endmacro
+; restores the registers
+%macro regrest 0
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+%endmacro
+
 %macro print 0
+  regsave
   mov ah, 0xE
   mov al, 0xD
   int 10h
   mov al, 0xA
   int 10h
+  regrest
 %endmacro
 
 %macro print 1
+  regsave
+  ; set the registers
   mov si, word %1
   mov ah, 0xE
 
@@ -20,6 +39,7 @@
     jmp %%print_char
 
   %%done:
+    regrest
 %endmacro
 
 ; vi: ft=nasm:ts=2:sw=2 expandtab
