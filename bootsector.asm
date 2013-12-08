9e15819433ae4bc54d8605455fb64f81600ae325
Szymon Urbaś <szymon.urbas@aol.com>
2013-12-08 18:17:58 +0100

    Change the intro/welcoming message

--- bootsector.asm
+++ bootsector.asm
@@ -3,6 +3,8 @@ ORG 0
 
 jmp 0x07C0:bootloader
 
+%include "utils.asm"
+
 bootloader:
   ; update the segment register
   mov ax, 0x07C0
@@ -18,48 +20,10 @@ bootloader:
   ; save the device's number from which we've booted
   mov [bootdrv], dl
 
-  mov si, welcome_msg
-  mov ah, 0xE
-
-  .print_char:
-    lodsb
-    cmp al, 0
-    je .done
-    int 10h
-    jmp .print_char
-
-  .done:
-    %macro increment_si 1
-      push cx
-      mov cx, %1
-      %%repeat:
-        cmp cx, 0
-        jle %%inc_done
-        inc si
-        dec cx
-        jmp %%repeat
-        %%inc_done:
-          pop cx
-    %endmacro
-
-    %macro printhex 1
-      mov si, hexadecimal
-      increment_si %1
-      mov ah, 0xE
-      mov al, byte [si]
-      int 10h
-    %endmacro
-
-    ; print the version
-    mov ch, 0
-    mov cl, [$$ + 509]
-    mov ax, cx
-    mov bx, 0x10
-    ; divide the version by 16
-    div bx
-    ; now, ax = ax / 16 (quotient); dx = ax % 16 (remainder)
-    printhex ax
-    printhex dx
+  print welcome_msg1
+  print welcome_msg2
+  print welcome_msg3
+  print welcome_msg4
 
 reset:
   ; reset the floppy drive
@@ -91,17 +55,17 @@ read:
   ; jump to stage two
   jmp 0500h:0000h
 
-; the string
-welcome_msg db 'Nihilum bootloader 0x', 0
-hexadecimal db '0123456789ABCDEF'
+; the strings
+welcome_msg1 db 'Welcome to Nihilum  v0.01  http://github.com/semahawk/nihilum', 0dh, 0ah, 0dh, 0ah, 0
+welcome_msg2 db '  Copyright (c) 2013 - Szymon Urbas', 0dh, 0ah, 0dh, 0ah, 0
+welcome_msg3 db 'The source code is licensed under the 3 Clause BSD License.', 0dh, 0ah, 0
+welcome_msg4 db 'For more licensing information, please visit the LICENSE file.', 0dh, 0ah, 0
 ; some variables
 sectors_read db 0
 bootdrv db 0
 
 ; pad the remainder of the boot sector with n's
-times 509-($-$$) db 0
-; the bootloaders version
-db 0x1
+times 510-($-$$) db 0
 ; the standard PC boot signature
 dw 0xAA55
 
