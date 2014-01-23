8b4b90dcb185004988bc5bcb65b372558c55f421
Szymon Urbaś <szymon.urbas@aol.com>
2014-01-23 19:01:34 +0100

    Enable the A20 line.

--- boot1.asm
+++ boot1.asm
@@ -10,6 +10,24 @@ boot1:
   mov ds, ax
   mov es, ax
 
+  ; enable the A20 line
+enable_a20:
+  ; try using BIOS first
+  mov ax, 0x2401
+  int 0x15
+  jnc .done
+  ; BIOS doesn't support INT 15, 2401
+  ; try using FAST A20
+  in al, 0x92
+  test al, 2
+  jnz .done
+  or al, 2
+  and al, 0xFE
+  out 0x92, al
+  ; if this doesn't work we're gonna have a problem
+
+.done: ; enable_a20
+
   cli
   hlt
 
