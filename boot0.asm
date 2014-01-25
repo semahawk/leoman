22d6a9a455cb9a0086b051d31faf64a405bda478
Szymon Urbaś <szymon.urbas@aol.com>
2014-01-25 21:01:12 +0100

    Move printing the menu to boot0
    Which gives us even more space in boot1!

--- boot0.asm
+++ boot0.asm
@@ -3,6 +3,8 @@ ORG 0
 
 jmp 0x07C0:boot0
 
+%include "utils.asm"
+
 boot0:
   ; update the segment register
   mov ax, 0x07C0
@@ -18,6 +20,10 @@ boot0:
   ; save the device's number from which we've booted
   mov [bootdrv], dl
 
+welcome:
+  ; print the welcomming message (d'oh!)
+  print welcome_msg
+
 ;
 ; MBR load (into 0090h:0000h)
 ;
@@ -26,8 +32,8 @@ reset_mbr:
   mov ah, 00h
   mov dl, [bootdrv]
   int 13h
-
-  jc reset_mbr      ; error -> try again
+  ; error -> try again
+  jc reset_mbr
 
 read_mbr:
   ; set up the registers
@@ -43,8 +49,86 @@ read_mbr:
   mov dh, 0         ; head no. 0
   mov dl, [bootdrv] ; the boot device
   int 13h           ; read!
+  ; error -> try again
+  jc read_mbr
 
-  jc read_mbr       ; error -> try again
+  ; print the partition's (types) located in the MBR
+  mov cx, 5       ; starting from 5 because of the 'dec cx'
+  mov ax, 0x0090
+  mov es, ax
+  mov si, 0x1C2   ; 1BEh + 4h
+  mov dl, 1       ; the partition's menu id
+
+  print_partitions:
+  dec cx
+  ; print the system's identification string
+  .print_sys_id:
+    cmp byte [es:si], 0x00    ; None
+    je .print_sys_id_after_nolf
+    ; print '['
+    mov ah, 0xE
+    mov al, '['
+    int 10h
+    ; print the partition's menu id
+    print_digit dl
+    ; print ']'
+    mov ah, 0xE
+    mov al, ']'
+    int 10h
+    ; print ' '
+    mov ah, 0xE
+    mov al, ' '
+    int 10h
+
+    cmp byte [es:si], 0xA5    ; FreeBSD
+    je .print_sys_id_bsd
+    cmp byte [es:si], 0xA6    ; OpenBSD
+    je .print_sys_id_bsd
+    cmp byte [es:si], 0x39    ; Plan 9
+    je .print_sys_id_plan9
+    cmp byte [es:si], 0x83    ; Linux (any)
+    je .print_sys_id_linux
+    cmp byte [es:si], 0x07    ; Windows
+    je .print_sys_id_windoze
+    ; none of the above matches the system's id
+    print os_unknown
+    jmp .print_sys_id_after
+
+  .print_sys_id_bsd:
+    print os_bsd
+    ; increment the partition's menu id
+    inc dl
+    jmp .print_sys_id_after
+  .print_sys_id_plan9:
+    print os_plan9
+    ; increment the partition's menu id
+    inc dl
+    jmp .print_sys_id_after
+  .print_sys_id_linux:
+    print os_linux
+    ; increment the partition's menu id
+    inc dl
+    jmp .print_sys_id_after
+  .print_sys_id_windoze:
+    print os_windoze
+    ; increment the partition's menu id
+    inc dl
+    jmp .print_sys_id_after
+
+  .print_sys_id_after:
+  call utils_print_newline
+  .print_sys_id_after_nolf:
+  ; 'go' to the next partition entry
+  add si, 16
+  ; it's kind of strange, but doing 'loop print_partitions' here gives me 'short
+  ; jump out of range' error
+  cmp cx, 0
+  jg print_partitions
+  ; print the additional menu items
+  print boot_from_hd_msg
+
+  ; save the number of systems
+  mov [syscount], dl
 
 ;
 ; Stage Two load (into 0050h:0000h)
@@ -74,13 +158,26 @@ read_boot1:
 
   jc read_boot1     ; error -> try again
 
+  ; save the system partition count (so boot1 can use it)
+  mov cl, [syscount]
   ; save the boot drive (so boot1 can use it)
   mov dl, [bootdrv]
   ; jump to stage two
   jmp 0050h:0000h
 
+; the messages
+welcome_msg db 'Quidquid Latine dictum, sit altum videtur.', 0xD, 0xA, 0xD, 0xA, 0
+boot_from_hd_msg db '[a] boot from HD', 0xD, 0xA, 0xD, 0xA, 0
+; the OS IDs
+os_bsd db 'BSD', 0
+os_plan9 db 'Plan 9', 0
+os_linux db 'GNU/Linux', 0
+os_windoze db 'Windows', 0
+os_unknown db 'unknown', 0
 ; number of the drive we have booted from
 bootdrv db 0
+; number of system partitions
+syscount db 0
 
 ; pad the remainder of the boot sector with zeros
 times 446-($-$$) db 0
--- boot1.asm
+++ boot1.asm
@@ -10,87 +10,8 @@ boot1:
   mov ds, ax
   mov es, ax
 
-welcome:
-  ; print the welcomming message (d'oh!)
-  print welcome_msg
-
-  ; print the partition's (types) located in the MBR
-  mov cx, 5       ; starting from 5 because of the 'dec cx'
-  mov ax, 0x0090
-  mov es, ax
-  mov si, 0x1C2   ; 1BEh + 4h
-  mov dl, 1       ; the partition's menu id
-
-print_partitions:
-  dec cx
-  ; print the system's identification string
-  .print_sys_id:
-    cmp byte [es:si], 0x00    ; None
-    je .print_sys_id_after_nolf
-    ; print '['
-    mov ah, 0xE
-    mov al, '['
-    int 10h
-    ; print the partition's menu id
-    print_digit dl
-    ; print ']'
-    mov ah, 0xE
-    mov al, ']'
-    int 10h
-    ; print ' '
-    mov ah, 0xE
-    mov al, ' '
-    int 10h
-
-    cmp byte [es:si], 0xA5    ; FreeBSD
-    je .print_sys_id_bsd
-    cmp byte [es:si], 0xA6    ; OpenBSD
-    je .print_sys_id_bsd
-    cmp byte [es:si], 0x39    ; Plan 9
-    je .print_sys_id_plan9
-    cmp byte [es:si], 0x83    ; Linux (any)
-    je .print_sys_id_linux
-    cmp byte [es:si], 0x07    ; Windows
-    je .print_sys_id_windoze
-    ; none of the above matches the system's id
-    print os_unknown
-    jmp .print_sys_id_after
-
-  .print_sys_id_bsd:
-    print os_bsd
-    ; increment the partition's menu id
-    inc dl
-    jmp .print_sys_id_after
-  .print_sys_id_plan9:
-    print os_plan9
-    ; increment the partition's menu id
-    inc dl
-    jmp .print_sys_id_after
-  .print_sys_id_linux:
-    print os_linux
-    ; increment the partition's menu id
-    inc dl
-    jmp .print_sys_id_after
-  .print_sys_id_windoze:
-    print os_windoze
-    ; increment the partition's menu id
-    inc dl
-    jmp .print_sys_id_after
-
-  .print_sys_id_after:
-  call utils_print_newline
-  .print_sys_id_after_nolf:
-  ; 'go' to the next partition entry
-  add si, 16
-  ; it's kind of strange, but doing 'loop print_partitions' here gives me 'short
-  ; jump out of range' error
-  cmp cx, 0
-  jg print_partitions
-  ; print the additional menu items
-  print boot_from_hd_msg
-
-  ; save the number of systems
-  mov [syscount], dl
+  ; save the # of system partitions
+  mov [syscount], cl
 
 keypress:
   ; wait for a key to be pressed
@@ -151,16 +72,7 @@ halt:
   ; stop right there!
   jmp $
 
-welcome_msg db 'Quidquid Latine dictum, sit altum videtur.', 0xD, 0xA, 0xD, 0xA, 0
-boot_from_hd_msg db '[a] boot from HD', 0xD, 0xA, 0xD, 0xA, 0
 booting_msg db 'booting...', 0xD, 0xA, 0
-
-os_bsd db 'BSD', 0
-os_plan9 db 'Plan 9', 0
-os_linux db 'GNU/Linux', 0
-os_windoze db 'Windows', 0
-os_unknown db 'unknown', 0
-
 ; number of system partitions
 syscount db 0
 
