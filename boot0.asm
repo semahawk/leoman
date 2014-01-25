36a7fc56f1ba5976b36a948b1c5ea3b0b43d7012
Szymon Urbaś <szymon.urbas@aol.com>
2014-01-25 16:11:44 +0100

    Put some exemplary MBR into boot0
    It's actually copied straight from phony-mbr.asm

--- boot0.asm
+++ boot0.asm
@@ -51,7 +51,81 @@ read:
 bootdrv db 0
 
 ; pad the remainder of the boot sector with zeros
-times 510-($-$$) db 0
+times 446-($-$$) db 0
+
+; partition entry #1 {{{
+; status (bit 7 set: active / bootable; old MBRs only accept 80h)
+;
+; 0x00: inactive
+; 0x01-0x7F: invalid
+db 0x00
+; CHS address of FIRST absolute sector in the partition
+db 00000000b   ; head (bits 0-7)
+db 00000000b   ; sector (bits 0-5); cylinder (bits 6-7 are bits 8-9 of
+               ; the cylinder)
+db 00000000b   ; cylinder (bits 0-7)
+; partition type
+db 0xA5        ; (FreeBSD's id)
+; CHS address of LAST absolute sector in the partition
+db 00000000b   ; head (bits 0-7)
+db 00000000b   ; sector (bits 0-5); cylinder (bits 6-7 are bits 8-9 of
+               ; the cylinder)
+db 00000000b   ; cylinder (bits 0-7)
+; LBA of first absolute sector in the partition
+dd 0x0
+; # of sectors in the partition
+dd 0x0
+; }}}
+; partition entry #2 {{{
+; status (bit 7 set: active / bootable; old MBRs only accept 80h)
+;
+; 0x00: inactive
+; 0x01-0x7F: invalid
+db 0x00
+; CHS address of FIRST absolute sector in the partition
+db 00000000b   ; head (bits 0-7)
+db 00000000b   ; sector (bits 0-5); cylinder (bits 6-7 are bits 8-9 of
+               ; the cylinder)
+db 00000000b   ; cylinder (bits 0-7)
+; partition type
+db 0x39        ; (Plan 9's id)
+; CHS address of LAST absolute sector in the partition
+db 00000000b   ; head (bits 0-7)
+db 00000000b   ; sector (bits 0-5); cylinder (bits 6-7 are bits 8-9 of
+               ; the cylinder)
+db 00000000b   ; cylinder (bits 0-7)
+; LBA of first absolute sector in the partition
+dd 0x0
+; # of sectors in the partition
+dd 0x0
+; }}}
+; partition entry #3 {{{
+; status (bit 7 set: active / bootable; old MBRs only accept 80h)
+;
+; 0x00: inactive
+; 0x01-0x7F: invalid
+db 0x00
+; CHS address of FIRST absolute sector in the partition
+db 00000000b   ; head (bits 0-7)
+db 00000000b   ; sector (bits 0-5); cylinder (bits 6-7 are bits 8-9 of
+               ; the cylinder)
+db 00000000b   ; cylinder (bits 0-7)
+; partition type
+db 0xA6        ; (OpenBSD's id)
+; CHS address of LAST absolute sector in the partition
+db 00000000b   ; head (bits 0-7)
+db 00000000b   ; sector (bits 0-5); cylinder (bits 6-7 are bits 8-9 of
+               ; the cylinder)
+db 00000000b   ; cylinder (bits 0-7)
+; LBA of first absolute sector in the partition
+dd 0x0
+; # of sectors in the partition
+dd 0x0
+; }}}
+; partition entry #4 {{{
+dq 0, 0
+; }}}
+
 ; the standard PC boot signature
 dw 0xAA55
 
