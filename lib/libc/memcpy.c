0925aa6d9bb4b86a782d47e34e76486ad9f05bec
Szymon Urbaś <szymon.urbas@aol.com>
2016-12-06 20:06:21 +0100

    Add the definition of memcpy to libc

---
+++ lib/libc/memcpy.c
@@ -0,0 +1,29 @@
+/*
+ *
+ * memcpy.c
+ *
+ * Created at:  06 Dec 2016 19:58:14 +0100 (CET)
+ *
+ * Author:  Szymon Urbaś <szymon.urbas@aol.com>
+ *
+ * License:  please visit the LICENSE file for details.
+ *
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+void *memcpy(void *dst, void *src, size_t len)
+{
+  void *ret = dst;
+
+  while (len-- != 0)
+    *(uint8_t *)dst++ = *(uint8_t *)src++;
+
+  return ret;
+}
+
+/*
+ * vi: ft=c:ts=2:sw=2:expandtab
+ */
+
