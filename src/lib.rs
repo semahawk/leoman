a8b2de7b0266810fc1862903593aa5f50df04d39
Szymon Urbaś <szymon.urbas@aol.com>
2017-01-06 21:12:46 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//
+// lib.rs
+// Copyright (C) 2017 Szymon Urbaś <szymon.urbas@aol.com>
+// Distributed under terms of the BSD (2-clause) license.
+//
+// Created on: 06 Jan 2017 21:05:59 +0100 (CET) 21:05
+//
+
+fn kmain() -> ! {
+  loop {}
+}
+
+/*
+ * vi: ts=2 sw=2 expandtab
+ */
+
