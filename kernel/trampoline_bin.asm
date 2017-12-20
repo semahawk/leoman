9e6966a5674bf0776623d6448c2826fe12cc6665
Szymon Urbaś <szymon.urbas@aol.com>
2017-12-20 19:10:01 +0100

    kernel: smp: make APs enter protected mode

--- kernel/trampoline_bin.asm
+++ kernel/trampoline_bin.asm
@@ -18,11 +18,59 @@ trampoline:
     mov bx, word [magic]
     mov cx, word [magic + 2]
 
+enter_protected_mode:
+    cli
+    lgdt [gdt]
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
+    jmp 0x08:protected_mode
+
+bits 32
+protected_mode:
+    mov eax, 0x10
+    mov ds, eax
+    mov es, eax
+    mov fs, eax
+    mov gs, eax
+    mov ss, eax
+
+    mov eax, dword [magic]
+
 .halt:
     cli
     hlt
 
     ; just for good measure
     jmp .halt
+;
+; The Global Descriptor Table
+;
+gdt_data:
+; {{{
+; the null selector
+  dq 0x0           ; nothing!
+
+; the code selector: base = 0x0, limit = 0xfffff
+  dw 0xffff        ; limit low (0-15)
+  dw 0x0           ; base low (0-15)
+  db 0x0           ; base middle (16-23)
+  db 10011010b     ; access byte
+  db 11001111b     ; flags + limit (16-19)
+  db 0x0           ; base high (24-31)
+
+; the data selector: base = 0x0, limit = 0xfffff
+  dw 0xffff        ; limit low (0-15)
+  dw 0x0           ; base low (0-15)
+  db 0x0           ; base middle (16-23)
+  db 10010010b     ; access byte
+  db 11001111b     ; flags + limit (16-19)
+  db 0x0           ; base high (24-31)
+; THE actual descriptor
+gdt_end:
+gdt:
+  dw gdt_end - gdt_data - 1 ; sizeof gdt
+  dd gdt_data
+; }}}
 
 ; vi: ft=nasm:ts=2:sw=2:expandtab
