18dbeb5202bab2974f3be5daba0767ee239f9779
Szymon Urbaś <szymon.urbas@aol.com>
2017-12-25 14:00:54 +0100

    kernel: ipc: Extract sending a message to a separate function: `ipc_send_msg`

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -14,6 +14,7 @@
 
 #include <kernel/common.h>
 #include <kernel/idt.h>
+#include <kernel/ipc.h>
 #include <kernel/pm.h>
 #include <kernel/proc.h>
 #include <kernel/syscall.h>
@@ -34,60 +35,7 @@ struct intregs *syscall_send_msg(struct intregs *regs)
     return regs;
   }
 
-  /*vga_printf("[ipc] proc %s sends a message to %s (state: %d)\n", current_proc->name, receiver->name, receiver->state);*/
-
-  switch (receiver->state){
-    case PROC_RECV_BLOCKED:
-      /* send-block the sender - it now has to wait for the response */
-      /* it's a way to eliminate busy-looping */
-      current_proc->state = PROC_SEND_BLOCKED;
-
-      void *mapped_recv_buf_base = (void *)0xdead0000;
-      void *mapped_recv_buf = (void *)((uint32_t)mapped_recv_buf_base + ((uint32_t)receiver->waiting_msg.recv_buf & 0xfff));
-
-      map_pages(receiver->waiting_msg.phys_recv_buf, mapped_recv_buf_base, 0, receiver->waiting_msg.recv_len);
-
-      memcpy(mapped_recv_buf, msg->send_buf, receiver->waiting_msg.recv_len);
-
-      unmap_pages(mapped_recv_buf_base, receiver->waiting_msg.recv_len);
-
-      void *mapped_msg_packet_base = (void *)0xbabe0000;
-      struct msg_packet *mapped_msg_packet = (void *)((uint32_t)mapped_msg_packet_base + ((uint32_t)receiver->waiting_msg.phys_msg_packet & 0xfff));
-
-      map_pages(receiver->waiting_msg.phys_msg_packet, mapped_msg_packet, 0, sizeof(struct msg_packet));
-
-      /* this is what's going to be returned in the receiver's ipc_recv call */
-      /* if we don't overwrite it here then it will think it got a message from
-       * an undefined process */
-      mapped_msg_packet->sender = current_proc->pid;
-      /* don't think there were two same messages */
-      receiver->waiting_msg.sender = -2;
-
-      unmap_pages(mapped_msg_packet, sizeof(struct msg_packet));
-
-      /* unblock the receiver so it can go and process the message */
-      receiver->state = PROC_READY;
-      break;
-    case PROC_READY:
-      receiver->waiting_msg.sender = current_proc->pid;
-
-      /* reply-block the sender - it now has to wait for the response */
-      /* it's a way to eliminate busy-looping */
-      current_proc->state = PROC_REPLY_BLOCKED;
-      break;
-    default:
-       vga_printf("[ipc] !! the receiver (%s) has an unknown state (%d)!\n",
-           receiver->name, receiver->state);
-      goto err;
-  }
-
-  receiver->waiting_msg.receiver = msg->receiver;
-  receiver->waiting_msg.send_buf = msg->send_buf;
-  receiver->waiting_msg.send_len = msg->send_len;
-  receiver->waiting_msg.recv_buf = msg->recv_buf;
-  receiver->waiting_msg.recv_len = msg->recv_len;
-  receiver->waiting_msg.phys_send_buf = vm_get_phys_mapping(msg->send_buf);
-  receiver->waiting_msg.phys_recv_buf = vm_get_phys_mapping(msg->recv_buf);
+  ipc_send_msg(msg, receiver);
 
   /* XXX short window for an interrupt to come? */
   proc_enable_scheduling();
