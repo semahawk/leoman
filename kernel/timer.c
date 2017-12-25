f2c56bd1726f80a9fd97a892ef344205ffd3f28a
Szymon Urbaś <szymon.urbas@aol.com>
2017-12-25 17:14:57 +0100

    kernel: Change the PIT frequency to 1000Hz

--- kernel/timer.c
+++ kernel/timer.c
@@ -43,10 +43,21 @@ void timer_wait(int ticks)
     __asm volatile("sti\n\thlt\n\tcli");
 }
 
+void timer_set_phase(int hz)
+{
+  int divisor = 1193180 / hz;     /* Calculate our divisor */
+
+  outb(0x43, 0x36);           /* Set our command byte 0x36 */
+  outb(0x40, divisor & 0xFF); /* Set low byte of divisor */
+  outb(0x40, divisor >> 8);   /* Set high byte of divisor */
+}
+
 /* set up the system clock by installing the timer handler into IRQ0 */
 void timer_install(void)
 {
   irq_install_handler(0, timer_handler, 0);
+
+  timer_set_phase(1000);
 
   vga_printf("[timer] timer was set up (irq 0)\n");
 }
